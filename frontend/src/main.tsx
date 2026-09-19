b2e4ca9aa2dea2d6dd1fcdb7350f5b0b8a183c8b
Farkoal <beilong.li@gmail.com>
2026-09-19 00:07:40 -0700

    Keep ingest session alive across routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { App } from './App'
 import './index.css'
+import { IngestSessionProvider } from './ingestSession'
 import { Ingest } from './pages/Ingest'
 import { JobProgress } from './pages/JobProgress'
 import { Review } from './pages/Review'
@@ -24,6 +25,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <IngestSessionProvider>
+      <RouterProvider router={router} />
+    </IngestSessionProvider>
   </React.StrictMode>,
 )
